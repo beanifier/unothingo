460ff01a408557c75e977a3df823bc753d75a41f
beanifier <entonival@gmail.com>
2025-04-28 20:41:36 +0100

    a

--- src/PlayWorstAI.java
+++ src/PlayWorstAI.java
@@ -1,11 +1,32 @@
-public class RandomAI extends PlayerAI {
-    public RandomAI() {
+public class PlayWorstAI extends PlayerAI {
+    public PlayWorstAI() {
         super();
+    }
+
+    public int cardValue(Card card) {
+        int value = 0;
+        switch (card.type) {
+            case WILD_DRAW4:
+                value = 500;
+                break;
+            case WILD:
+                value = 400;
+                break;
+            case SKIP:
+                value = 300;
+                break;
+        }
+        return value;
+    }
+
+    public void sort() {
+        // Bubble sort
+        
     }
 
     @Override
     public GameMove playTurn(GameMove lastMove) throws Exception {
-        player.hand.shuffle();
+        sort();
         GameMove move = new GameMove();
         int index = -1;
         for (int i = 0; i < player.hand.size(); i++) {
--- src/PlayerAI.java
+++ src/PlayerAI.java
@@ -24,4 +24,18 @@ public abstract class PlayerAI {
     public void reverseEvent(GameMove move) {
         return;
     }
+
+    public boolean isCardIdLegal(int cardId) throws Exception {
+        return player.game.isMoveLegal(new GameMove(player, cardId));
+    }
+
+    public Card card(int id) {
+        return player.hand.getCard(id);
+    }
+
+    public void chooseRandomWild(Card card) {
+        if (Card.isWild(card)) {
+            card.color = CardColor.values()[(int) (Math.random() * 4)];
+        }
+    }
 }
